44f56a84e7160b2d9c5ee5361aace9487294b8a8
Mohammad Attar <mhammad.attar@gmail.com>
2022-11-16 10:17:59 +0200

    Update config_api.dart

--- lib/config/config_api.dart
+++ lib/config/config_api.dart
@@ -1,4 +1,5 @@
 class ConfigAPI {
+  //Example: https://www.example.com without /
   static const String basrUrl = 'YOUR_BASE_PASS_APP';
   static const int sessionTimeoutThreshold =
       0; // Will refresh the access token 5 minutes before it expires
